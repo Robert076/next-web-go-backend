ec6facb52f141f7d14af6a6d908b8835a22c8e4b
Robert076 <robertberes06@gmail.com>
2025-04-08 13:19:25 +0300

    done, working

--- backend_service/api.go
+++ backend_service/api.go
@@ -7,6 +7,8 @@ import (
 
 func main() {
 	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode("Hello, World!")
 	})
 	http.ListenAndServe(":8080", nil)
--- web_service/app/page.tsx
+++ web_service/app/page.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const [data, setData] = useState<string>("");
+
+  const fetchData = async () => {
+    const response = await fetch("http://localhost:8080/api");
+    const result = await response.json();
+    setData(result);
+  };
+
+  return (
+    <div>
+      <button onClick={fetchData}>Click me</button>
+      {data}
+    </div>
+  );
 };
 
 export default HomePage;
